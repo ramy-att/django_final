e5bcbb3827dec76686e6fd4d41aef4e8c8cebc4c
Ramy Attalla <ramysamerattalla@gmail.com>
2021-12-27 23:45:04 -0500

    First commit

--- django_final/settings.py
+++ django_final/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!1cc&o6y#m+%-4za&mjz013812ciqak5wyr3d#gd+tqac4d)o2'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'oweugnweufgnw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
